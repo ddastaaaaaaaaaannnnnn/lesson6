4109ff65c204c950c4ff7cd00cd1b8fdf4c84038
ddastaaaaaaaaaannnnnn <dastanskiii@gmail.com>
2022-01-02 20:04:07 +0300

    Initial commit

---
+++ app/src/main/java/com/example/lesson6/interfaces/OnItemClickListener.java
@@ -0,0 +1,8 @@
+package com.example.lesson6.interfaces;
+
+
+import com.example.lesson6.data.GameModel;
+
+public interface OnItemClickListener {
+    void OnItemClick(int position, GameModel gameModel);
+}
